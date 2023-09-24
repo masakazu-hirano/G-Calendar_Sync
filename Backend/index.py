03f59e4dfe6a799ccaf741de7dcdbd36cf961b1c
mskzu-hirno <63486456+mskzu-hirno@users.noreply.github.com>
2023-09-24 16:09:33 +0900

    Notion API にリクエストする際の条件をコンポーネント化

--- Backend/index.py
+++ Backend/index.py
@@ -6,6 +6,7 @@ import requests
 
 from datetime import datetime
 from dotenv import load_dotenv
+from Components.Notion_API import SET_Header, SET_Search_Condition
 
 class Notion_DB_Record:
 	def __init__(self, id: str, title: str, category: str, start_date: datetime, end_date: datetime, created: datetime, updated: datetime):
@@ -17,54 +18,29 @@ class Notion_DB_Record:
 		self.created: datetime = created
 		self.updated: datetime = updated
 
-def Set_Header_Information() -> dict:
-	return {
-		'Authorization': f"Bearer {os.getenv(key = 'NOTION_SECRET_KEY')}",
-		'accept': 'application/json',
-		'content-type': 'application/json',
-		'Notion-Version': '2022-06-28'
-	}
-
 def Get_Notion_DB_Record() -> list[Notion_DB_Record]:
 	loop_count: int = 0
-	record_list: list[Notion_DB_Record] = []
 	has_more: bool = True
+	notion_db_url: str = f"https://api.notion.com/v1/databases/{os.getenv(key = 'NOTION_DB_ID')}/query"
+	record_list: list[Notion_DB_Record] = []
 	while has_more:
 		if loop_count == 0:
 			response = requests.post(
-				url = f"https://api.notion.com/v1/databases/{os.getenv(key = 'NOTION_DB_ID')}/query",
-				headers = Set_Header_Information(),
-				json = {
-					'page_size': 100,
-					'filter': {
-						'property': 'Category',
-						'select': { 'equals': 'Calendar（スケジュール管理）' }
-					},
-					'sorts': [
-						{'property': '日時', 'direction': 'ascending'}
-					]
-				},
+				url = notion_db_url,
+				headers = json.loads(s = SET_Header()),
+				json = json.loads(s = SET_Search_Condition()),
 			)
 		elif loop_count > 0:
 			response = requests.post(
-				url = f"https://api.notion.com/v1/databases/{os.getenv(key = 'NOTION_DB_ID')}/query",
-				headers = Set_Header_Information(),
-				json = {
-					'start_cursor': start_cursor,
-					'page_size': 100,
-					'filter': {
-						'property': 'Category',
-						'select': { 'equals': 'Calendar（スケジュール管理）' }
-					},
-					'sorts': [
-						{'property': '日時', 'direction': 'ascending'}
-					]
-				},
+				url = notion_db_url,
+				headers = json.loads(s = SET_Header()),
+				json = json.loads(s = SET_Search_Condition(start_cursor = start_cursor))
 			)
 
 		if response.status_code == 200:
-			for result in json.loads(response.text)['results']:
+			for result in json.loads(s = response.text)['results']:
 				try:
+					# 日時に、終了時刻が設定されている場合
 					record_list.append(
 						Notion_DB_Record(
 							id = result['id'],
@@ -77,6 +53,7 @@ def Get_Notion_DB_Record() -> list[Notion_DB_Record]:
 						)
 					)
 				except TypeError:
+					# 終日予定（終了時刻 未設定）の場合
 					record_list.append(
 						Notion_DB_Record(
 							id = result['id'],
@@ -89,14 +66,14 @@ def Get_Notion_DB_Record() -> list[Notion_DB_Record]:
 						)
 					)
 
-			if json.loads(response.text)['has_more']:
-				start_cursor = json.loads(response.text)['next_cursor']
+			if json.loads(s = response.text)['has_more']:
+				start_cursor = json.loads(s = response.text)['next_cursor']
 				loop_count += 1
 			else:
-				has_more = json.loads(response.text)['has_more']
+				has_more = json.loads(s = response.text)['has_more']
 		else:
-			logging.error(msg = f"ステータスコード: {json.loads(response.text)['status']}")
-			logging.error(msg = f"エラーメッセージ: {json.loads(response.text)['message']}")
+			logging.error(msg = f"ステータスコード: {json.loads(s = response.text)['status']}")
+			logging.error(msg = f"エラーメッセージ: {json.loads(s = response.text)['message']}")
 			break
 
 	return record_list
