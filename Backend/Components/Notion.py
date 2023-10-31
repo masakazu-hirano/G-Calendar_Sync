97b05756433d5fad2f8a9f6140c4aebc4dcee8e8
masakazu-hirano <63486456+masakazu-hirano@users.noreply.github.com>
2023-11-01 06:13:13 +0900

    NotionDB に新規追加したレコードを取得する条件を設定

--- Backend/Components/Notion.py
+++ Backend/Components/Notion.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+from datetime import datetime
+
 def SET_Header() -> str:
 	return json.dumps(
 		obj = {
@@ -12,6 +14,49 @@ def SET_Header() -> str:
 
 		indent = 4,
 		sort_keys = True,
+		ensure_ascii = False,
+		allow_nan = False,
+		skipkeys = False
+	)
+
+def SET_Conditions(current_datetime: datetime, start_cursor: str = None) -> str:
+	conditions: dict = {
+		'page_size': 100,
+		'sorts': [
+			{'property': '日時', 'direction': 'ascending'},
+			{'property': 'タイトル', 'direction': 'ascending'},
+		],
+
+		'filter': {
+			'and': [
+				{
+					'property': 'Category',
+					'multi_select': {'contains': 'Calendar（スケジュール管理）'}
+				},
+				{
+					'property': '日時',
+					'date': {'after': current_datetime.replace(
+						day = 1,
+						hour = 0,
+						minute = 0,
+						second = 0
+					).isoformat()}
+				},
+				{
+					'property': 'Google カレンダー（ID）',
+					'rich_text': {'is_empty': True}
+				}
+			]
+		}
+	}
+
+	if start_cursor != None:
+		conditions['start_cursor'] = start_cursor
+
+	return json.dumps(
+		obj = conditions,
+		indent = 4,
+		sort_keys = False,
 		ensure_ascii = False,
 		allow_nan = False,
 		skipkeys = False
