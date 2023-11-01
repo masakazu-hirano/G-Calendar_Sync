b8f511c0f6814b97b703966e58693bc3620a76fb
masakazu-hirano <63486456+masakazu-hirano@users.noreply.github.com>
2023-11-02 04:21:13 +0900

    Notion に予定を新規登録した際に取得する処理を追加

--- Backend/G-Calendar_Sync.py
+++ Backend/G-Calendar_Sync.py
@@ -2,14 +2,53 @@ import json
 import logging
 import os
 import pytz
-import requests
 
 from datetime import datetime
 from dotenv import load_dotenv
-from googleapiclient import discovery
-from google.oauth2.service_account import Credentials
+from requests import post, Response
 
-from Components.Create import Calendar_Regist
+from Components.Google import GET_Google_Credential
+from Components.Notion import SET_Header as Notion_Request_Header
+from Components.Notion import SET_Conditions
+
+def Create_Events(current_datetime: datetime):
+	notion_url: str = f"https://api.notion.com/v1/databases/{os.getenv(key = 'NOTION_DB_ID')}/query"
+
+	loop_count: int = 0
+	has_more: bool = True
+	while has_more:
+		if loop_count == 0:
+			response: Response = post(
+				url = notion_url,
+				headers = json.loads(s = Notion_Request_Header()),
+				json = json.loads(s = SET_Conditions(current_datetime)),
+			)
+		elif loop_count > 0:
+			response: Response = post(
+				url = notion_url,
+				headers = json.loads(s = Notion_Request_Header()),
+				json = json.loads(s = SET_Conditions(current_datetime, start_cursor)),
+			)
+
+		if response.status_code == 200:
+			notion_records = json.loads(s = response.text)
+			for record in notion_records['results']:
+				logging.info(msg = record)
+				break
+
+			if notion_records['has_more']:
+				start_cursor = notion_records['next_cursor']
+				loop_count += 1
+			else:
+				has_more = notion_records['has_more']
+		else:
+			logging.info(msg = json.loads(s = response.text))
+			has_more = False
+
+	return None
+
+def Update_Events():
+	return None
 
 if __name__ == '__main__':
 	load_dotenv()
@@ -22,48 +61,22 @@ if __name__ == '__main__':
 	time_zone = pytz.timezone(zone = 'Asia/Tokyo')
 	current_datetime: datetime = datetime.now().astimezone(tz = time_zone).replace(microsecond = 0)
 
-	notion_request_header: dict[str, str] = {
-		'content-type': 'application/json',
-		'accept': 'application/json',
-		'Authorization': f"Bearer {os.getenv(key = 'NOTION_SECRET_KEY')}",
-		'Notion-Version': '2022-06-28'
-	}
-
-	with open(
-		file = '/usr/local/src/Google-Cloud_Secrets.json',
-		mode = 'r',
-		encoding = 'utf_8',
-		newline = '\n',
-		buffering = 1,
-		closefd = True
-	) as secrets:
-		google_client = discovery.build(
-			serviceName = 'calendar',
-			version = 'v3',
-			credentials = Credentials.from_service_account_info(
-				info = json.load(fp = secrets),
-				scopes = ['https://www.googleapis.com/auth/calendar.events'],
-			),
-
-			cache_discovery = False,
-			num_retries = 3
-		)
-
-	regist_count = Calendar_Regist(google_client, current_datetime)
-	requests.post(
-		url = 'https://api.line.me/v2/bot/message/push',
-		headers = {
-			'content-type': 'application/json',
-			'Authorization': f"Bearer {os.getenv(key = 'LINE_CHANNEL_ACCESS_TOKEN')}",
-		},
-		json = {
-			'to': os.getenv(key = 'LINE_USER_ID'),
-			'notificationDisabled': True,
-			'messages': [{
-				'type': 'text',
-				'text': f"■ Google カレンダーに同期完了\n　──────────\n　■ 実行日: {current_datetime.replace(microsecond = 0)}\n　■ 新規登録: {regist_count}件"
-			}]
-		}
-	)
+	Create_Events(current_datetime)
+	# regist_count = Calendar_Regist(google_client, current_datetime)
+	# requests.post(
+	# 	url = 'https://api.line.me/v2/bot/message/push',
+	# 	headers = {
+	# 		'content-type': 'application/json',
+	# 		'Authorization': f"Bearer {os.getenv(key = 'LINE_CHANNEL_ACCESS_TOKEN')}",
+	# 	},
+	# 	json = {
+	# 		'to': os.getenv(key = 'LINE_USER_ID'),
+	# 		'notificationDisabled': True,
+	# 		'messages': [{
+	# 			'type': 'text',
+	# 			'text': f"■ Google カレンダーに同期完了\n　──────────\n　■ 実行日: {current_datetime.replace(microsecond = 0)}\n　■ 新規登録: {regist_count}件"
+	# 		}]
+	# 	}
+	# )
 
 	logging.info(msg = '処理が正常に終了しました。')
